baf9b526b9425191682a96d5e6548ed6af85dd45
quoc anh le <lequocanh662000@gmail.com>
2023-01-30 12:09:50 +0700

    [SRE-66] check UNITY_LICENSE exists v5.0

--- .gitlab-ci/scripts/before_script.sh
+++ .gitlab-ci/scripts/before_script.sh
@@ -4,7 +4,7 @@ set -e #  use to exit when the command exits with a non-zero status
 set -x # Turn on debug
 mkdir -p $CI_PROJECT_DIR/.cache/unity3d
 mkdir -p $CI_PROJECT_DIR/.local/share/unity3d/Unity/
-set +x # Turn off debug
+
 
 unity_license_destination=/root/.local/share/unity3d/Unity/Unity_lic.ulf
 android_keystore_destination=keystore.keystore
@@ -24,10 +24,12 @@ then
 fi
 
 if [ -n "$UNITY_LICENSE" ]
-then
-    touch $CI_PROJECT_DIR/.local/share/unity3d/Unity/Unity_lic.ulf
+    sudo touch unity_license_destination
     echo "Writing '\$UNITY_LICENSE' to license file ${unity_license_destination}"
-    echo "${UNITY_LICENSE}" | tr -d '\r' > ${unity_license_destination}
+    # echo "${UNITY_LICENSE}" | tr -d '\r' > ${unity_license_destination}
+    sudo bash -c "echo '${UNITY_LICENSE}' | tr -d '\r' > ${unity_license_destination}"
 else
     echo "'\$UNITY_LICENSE' env var not found"
 fi
+
+set +x # Turn off debug
