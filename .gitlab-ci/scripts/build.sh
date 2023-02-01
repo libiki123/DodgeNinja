73f6135f8a22e0fbcf87376b0d5f9ca307cd45fe
quoc anh le <lequocanh662000@gmail.com>
2023-02-01 15:35:54 +0700

    Test Build on Mac v8.6 put logs into artifact

--- .gitlab-ci/scripts/build.sh
+++ .gitlab-ci/scripts/build.sh
@@ -23,20 +23,20 @@ touch $UNITY_DIR/Log/log.txt
   -customBuildTarget $BUILD_TARGET \
   -customBuildName $BUILD_NAME \
   -customBuildPath $BUILD_PATH \
-  -executeMethod BuildCommand.PerformBuild \
+  -executeMethod BuildCommand.PerformBuild > log.txt
   # -logFile $UNITY_DIR/Log/log.txt
 
-UNITY_EXIT_CODE=$?
+# UNITY_EXIT_CODE=$?
 
-if [ $UNITY_EXIT_CODE -eq 0 ]; then
-  echo "Run succeeded, no failures occurred";
-elif [ $UNITY_EXIT_CODE -eq 2 ]; then
-  echo "Run succeeded, some tests failed";
-elif [ $UNITY_EXIT_CODE -eq 3 ]; then
-  echo "Run failure (other failure)";
-else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
-fi
+# if [ $UNITY_EXIT_CODE -eq 0 ]; then
+#   echo "Run succeeded, no failures occurred";
+# elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+#   echo "Run succeeded, some tests failed";
+# elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+#   echo "Run failure (other failure)";
+# else
+#   echo "Unexpected exit code $UNITY_EXIT_CODE";
+# fi
 
 ls -la $BUILD_PATH
 [ -n "$(ls -A $BUILD_PATH)" ] # fail job if build folder is empty
