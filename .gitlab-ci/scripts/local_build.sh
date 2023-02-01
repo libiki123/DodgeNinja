1c78cf6f69f27de3f5fe534b5425700f288fc731
quoc anh le <lequocanh662000@gmail.com>
2023-02-01 10:45:19 +0700

    Test Build on Mac

---
+++ .gitlab-ci/scripts/local_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Hub/Editor/2021.3.15f1/Unity.app"}
+
+BUILD_TARGET=StandaloneLinux64 ./ci/build.sh
+
