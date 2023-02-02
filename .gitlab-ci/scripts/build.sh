03eb8eb217e48c19b358c3bd8cf0e73668b7315c
quoc anh le <lequocanh662000@gmail.com>
2023-02-02 10:00:59 +0700

    Test Build on Mac v9.2 fix Packages importation

--- .gitlab-ci/scripts/build.sh
+++ .gitlab-ci/scripts/build.sh
@@ -26,17 +26,17 @@ touch $UNITY_DIR/Log/log.txt
   -executeMethod BuildCommand.PerformBuild > logfromhome.txt
   # -logFile $UNITY_DIR/Log/log.txt
 
-UNITY_EXIT_CODE=$?
+# UNITY_EXIT_CODE=$?
 
-if [ $UNITY_EXIT_CODE -eq 0 ]; then
-  echo "Run succeeded, no failures occurred";
-elif [ $UNITY_EXIT_CODE -eq 2 ]; then
-  echo "Run succeeded, some tests failed";
-elif [ $UNITY_EXIT_CODE -eq 3 ]; then
-  echo "Run failure (other failure)";
-else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
-fi
+# if [ $UNITY_EXIT_CODE -eq 0 ]; then
+#   echo "Run succeeded, no failures occurred";
+# elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+#   echo "Run succeeded, some tests failed";
+# elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+#   echo "Run failure (other failure)";
+# else
+#   echo "Unexpected exit code $UNITY_EXIT_CODE";
+# fi
 
-ls -la $BUILD_PATH
-[ -n "$(ls -A $BUILD_PATH)" ] # fail job if build folder is empty
+# ls -la $BUILD_PATH
+# [ -n "$(ls -A $BUILD_PATH)" ] # fail job if build folder is empty
